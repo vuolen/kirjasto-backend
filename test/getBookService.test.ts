ab0bd9b06ad05c2577e0f26c8912a87fc79cfd72
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2021-01-07 13:29:13 +0200

    refactored getBookService tests and made a small fix in IOTaskEither

--- test/getBookService.test.ts
+++ test/getBookService.test.ts
@@ -1,22 +1,24 @@
 import * as E from "fp-ts/lib/Either"
-import { flow } from "fp-ts/lib/function"
 import * as IOTE from "../src/types/IOTaskEither"
-import { ServiceResponse } from "../src/main"
 import { getBookService } from "../src/services/getBookService"
 
-const getRightOrFail = <E, A>(a: E.Either<E, A>): A => {
+import Either = E.Either
+
+const getRightOrFail = <E, A>(a: Either<E, A>): A => {
     expect(E.isRight(a)).toBeTruthy()
     return (a as E.Right<A>).right
 }
 
 // TODO: remove the clash of the type parameter E and the alias import Either as E
-const getLeftOrFail = <E, A>(e: E.Either<E, A>): E => {
+const getLeftOrFail = <E, A>(e: Either<E, A>): E => {
     expect(E.isLeft(e)).toBeTruthy()
     return (e as E.Left<E>).left
 }
 
+const VALID_BOOK = {id: 1, title: "Test Book"}
+
 test("getBookService returns empty array as body when no books exist", done => {
-    const mockDb = {getBooks: IOTE.fromEither(E.right([]))}
+    const mockDb = {getBooks: IOTE.right([])}
     getBookService(mockDb)()().then(
         either => {
             const res = getRightOrFail(either)
@@ -26,8 +28,19 @@ test("getBookService returns empty array as body when no books exist", done => {
     )
 })
 
+test("getBookService returns correct book as body when a book exist", done => {
+    const mockDb = {getBooks: IOTE.right([VALID_BOOK])}
+    getBookService(mockDb)()().then(
+        either => {
+            const res = getRightOrFail(either)
+            expect(res.body).toEqual([VALID_BOOK])
+            done()
+        }
+    )
+})
+
 test("getBookService returns error when database gives an error", done => {
-    const mockDb = {getBooks: IOTE.fromEither(E.left(new Error("Database error")))}
+    const mockDb = {getBooks: IOTE.left(new Error("Database error"))}
     getBookService(mockDb)()().then(
         either => {
             const res = getLeftOrFail(either)
@@ -39,7 +52,7 @@ test("getBookService returns error when database gives an error", done => {
 })
 
 test("getBookService returns no status code when there is no error", done => {
-    const mockDb = {getBooks: IOTE.fromEither(E.right([]))}
+    const mockDb = {getBooks: IOTE.right([])}
     getBookService(mockDb)()().then(
         either => {
             const res = getRightOrFail(either)
