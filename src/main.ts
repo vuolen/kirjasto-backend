7b7d635f4f4372cc64427b2f362e5743254f3994
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2021-01-15 00:40:53 +0200

    Added authorization check on POST /books

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,21 @@ import { flow, pipe } from 'fp-ts/lib/function'
 import { createDatabaseHandle, DatabaseHandle } from './db'
 import { getBookService } from './services/getBookService'
 import * as IOTE from './types/IOTaskEither'
-import * as TE from 'fp-ts/lib/TaskEither'
+import * as E from 'fp-ts/lib/Either'
 import * as T from 'fp-ts/lib/Task'
 import * as I from 'fp-ts/lib/IO'
+import * as jwt from 'express-jwt'
+import * as jwksRsa from 'jwks-rsa'
+
 
 import IO = I.IO
 import { addBookService } from './services/addBookService'
+import JwksRsa = require('jwks-rsa')
 
 export interface ServiceResponse {
     statusCode?: number,
     body: any
 }
-
 const PORT = process.env.PORT || 8000
 
 const main: IO<void> =
@@ -49,21 +52,37 @@ function createExpress(db: DatabaseHandle) {
                 }
             )
         ),
-        app => app.post("/books", (req, res) => 
-            pipe(
-                req.body,
-                addBookService(db),
-                IOTE.fold(
-                    err => ({body: {error: "Internal server error: " + err}, statusCode: 500}),
-                    right => ({body: right.body, statusCode: right.statusCode || 200})
-                ),
-                iote => iote()()
-            ).then(
-                response => {
-                    res.statusCode = response.statusCode
-                    res.json(response.body)
-                }
-            )
+        app => app.post("/books",
+            jwt({secret: jwksRsa.expressJwtSecret({
+                jwksUri: "https://kirjasto-e2e.eu.auth0.com/.well-known/jwks.json"
+            }), algorithms: ["RS256"]}),
+            (req, res) => 
+                pipe(
+                    req.user.scope.includes("add:book") ? E.right(req.body) : E.left("Invalid permissions: add:book"),
+                    asd => {
+                        console.log(req.user)
+                        return asd
+                    },
+                    E.map(
+                        flow(
+                            addBookService(db),
+                            IOTE.fold(
+                                err => ({body: {error: "Internal server error: " + err}, statusCode: 500}),
+                                right => ({body: right.body, statusCode: right.statusCode || 200})
+                            ),
+                            iote => iote()()
+                        )
+                    ),
+                    E.fold(
+                        err => Promise.resolve({body: {error: err}, statusCode: 401}),
+                        right => right
+                    )
+                ).then(
+                    response => {
+                        res.statusCode = response.statusCode
+                        res.json(response.body)
+                    }
+                )
         )
     )
 }
